15150864c569fda3519c0c693387b8a9d77be464
DannyCroDev <danielrajic26@outlook.com>
2024-02-26 17:20:21 +0100

    fixed routing bug, dynamic urls,. error 404 working fully, finished its design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ export default function Navbar() {
        <h2>Navbar</h2>
       <ul className="navbarList">
         <li className="navbarList-item"><Link to="/">00 Home</Link></li>
-        <li className="navbarList-item"><Link to="destination">01 DESTINATION</Link></li>
-        <li className="navbarList-item"><Link to="crew">02 CREW</Link></li>
-        <li className="navbarList-item"><Link to="technology">03 TECHNOLOGY</Link></li>
+        <li className="navbarList-item"><Link to="/destination">01 DESTINATION</Link></li>
+        <li className="navbarList-item"><Link to="/crew">02 CREW</Link></li>
+        <li className="navbarList-item"><Link to="/technology">03 TECHNOLOGY</Link></li>
       </ul>
      
     </div>
