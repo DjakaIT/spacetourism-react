ee69307784e01f8d3af780fac4328403662c5b2b
DannyCroDev <danielrajic26@outlook.com>
2024-02-26 19:59:27 +0100

    css fixes, more to work on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='navMainDiv'>
-       <h2>Navbar</h2>
-      <ul className="navbarList">
-        <li className="navbarList-item"><Link to="/">00 Home</Link></li>
-        <li className="navbarList-item"><Link to="/destination">01 DESTINATION</Link></li>
-        <li className="navbarList-item"><Link to="/crew">02 CREW</Link></li>
-        <li className="navbarList-item"><Link to="/technology">03 TECHNOLOGY</Link></li>
-      </ul>
-     
+    <div className='navbarDiv'>
+       <nav className="navbar">
+        	<img src="public/assets/logo_48x48.png" alt="starry logo for navbar"  className='navbar-logo'/>
+          <div className='navbar-dash'></div>
+          <ul className="navbarList">
+            <li className="navbarList-item"><Link to="/" className='navbar-link'><b>00</b> HOME</Link></li>
+            <li className="navbarList-item"><Link to="/destination" className='navbar-link'><b>01</b> DESTINATION</Link></li>
+            <li className="navbarList-item"><Link to="/crew" className='navbar-link'><b>02</b> CREW</Link></li>
+            <li className="navbarList-item"><Link to="/technology" className='navbar-link'><b>03</b> TECHNOLOGY</Link></li>
+          </ul>
+       </nav>
     </div>
   )
 }
+
+
