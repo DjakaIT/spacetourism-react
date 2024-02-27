b39ed71291c450aab4a72b81c83f0ac3b7b65190
DannyCroDev <danielrajic26@outlook.com>
2024-02-27 19:40:59 +0100

    responsive css fixes/additions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import '../css/navbar.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import '../css/navbar.css';
 
 export default function Navbar() {
   return (
     <div className='navbarDiv'>
-       <nav className="navbar">
-        	<img src="public/assets/logo_48x48.png" alt="starry logo for navbar"  className='navbar-logo'/>
-          <div className='navbar-dash'></div>
-          <ul className="navbarList">
-            <li className="navbarList-item"><Link to="/" className='navbar-link'><b>00</b> HOME</Link></li>
-            <li className="navbarList-item"><Link to="/destination" className='navbar-link'><b>01</b> DESTINATION</Link></li>
-            <li className="navbarList-item"><Link to="/crew" className='navbar-link'><b>02</b> CREW</Link></li>
-            <li className="navbarList-item"><Link to="/technology" className='navbar-link'><b>03</b> TECHNOLOGY</Link></li>
-          </ul>
-       </nav>
+      <nav className="navbar">
+        <img src="public/assets/logo_48x48.png" alt="starry logo for navbar" className='navbar-logo' />
+        <div className='navbar-dash'></div>
+        <ul className="navbarList">
+          <li className="navbarList-item">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
+            >
+              <b>00</b> HOME
+            </NavLink>
+          </li>
+          <li className="navbarList-item">
+            <NavLink
+              to="/destination"
+              className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
+            >
+              <b>01</b> DESTINATION
+            </NavLink>
+          </li>
+          <li className="navbarList-item">
+            <NavLink
+              to="/crew"
+              className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
+            >
+              <b>02</b> CREW
+            </NavLink>
+          </li>
+          <li className="navbarList-item">
+            <NavLink
+              to="/technology"
+              className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
+            >
+              <b>03</b> TECHNOLOGY
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
-
-
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,16 +9,13 @@ export default function Home() {
 
   return(
     <div className="homeDiv">
-        <Navbar />
+        <Navbar />                                                                                                                                                                                                                                                                                                                                                                                          
             <div className="overlay-container">
-                <section className="home-text-container" data-aos="fade-up-right">
+                <section className="home-text-container">
                     <h5>So, you want to travel to</h5>
                     <h1>Space</h1>
                     <p className="description">
-                        Let`s face it; if you want to go to space, you might as well
-                        genuinely go to outer space and not hover kind of on the edge of it.
-                        Well sit back, and relax because we`ll give you a truly out of this
-                        world experience!
+                    Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
                     </p>
                 </section>
                 <section className="explore-btn-container" >
@@ -33,3 +30,9 @@ export default function Home() {
   
   
 }
+
+
+ 
+
+
+
